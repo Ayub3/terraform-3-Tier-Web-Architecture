02c3ec0ef50df42f06c5f1d95f7f61e8e3cc1354
Ayub Mohamud <a.mohamud1@hotmail.com>
2022-10-03 20:10:26 +0100

    add rds db to vpc

--- providers.tf
+++ providers.tf
@@ -12,6 +12,22 @@ terraform {
   }
 }
 
+# terraform {
+#   required_version = ">= 0.13.1"
+
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 4.6"
+#     }
+
+#      null = {
+#       source  = "hashicorp/null"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
+
 provider "aws" {
   region = var.aws_region
 }
